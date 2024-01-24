efdeeff9227c0768fd8b939827a59af29d7caa24
Maxwell Klema <maxklema@gmail.com>
2024-01-24 02:06:40 -0500

    new listener to remove broad host permissions from manifest

--- src/background/background.js
+++ src/background/background.js
@@ -1,3 +1,13 @@
+// src/background/background.js
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ["dist/bundle.js", "src/content/grabWebURL.js", "dist/bundle2.js"],
+        allFrames: true
+    });
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponce) {
 
     if (request.action === 'updatePopup') {
