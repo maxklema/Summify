de714791a31f7dd18c504e23261f95536530a111
maxklema <maxklema@gmail.com>
2025-05-27 23:43:33 -0400

    yt iframe updates, re-prepending on new video

--- src/content/youtubeImbed.js
+++ src/content/youtubeImbed.js
@@ -1,6 +1,41 @@
 var videoID = window.location.href;
 videoID = videoID.substring(videoID.indexOf("v=")+2, videoID.length)
 const iframe = document.createElement('iframe');
+var video; // Interact with video player on YouTube
+var iframePrepened = false //set to true when iframe successfully loads
+
+//Mutation Observers
+const callback = (mutationsList, observer) => {
+  mutationsList.forEach(mutation => {
+    const secondary = document.querySelector("#secondary");
+    if (secondary && !iframePrepened){
+      iframePrepened = true
+      setTimeout(() => {
+        secondary.prepend(iframe);
+      }, 4000)
+      observer.disconnect();  
+    }
+  })
+};
+
+const playerCallback = (mutationsList, observer) => {
+  mutationsList.forEach(mutation => {
+      video = document.querySelector('video')
+      if (video){
+        observer.disconnect()
+      }    
+  })
+}
+
+const config = { attributes: true, childList: true, subtree: true };
+var observer = new MutationObserver(callback);
+var playerObserver = new MutationObserver(playerCallback);
+
+observer.observe(document.getElementById("content"), config);
+playerObserver.observe(document.getElementById("content"), config);
+
+
+// Iframe
 iframe.setAttribute('id', 'summify-frame');
 iframe.setAttribute(
   'style',
@@ -10,47 +45,34 @@ iframe.setAttribute('allow', '');
 iframe.src = 'http://127.0.0.1:5000/yt_url/' + videoID;
 
 
-const callback = (mutationsList, observer) => {
-  mutationsList.forEach(mutation => {
-    switch(mutation.type) {
-        case 'childList':
-          const secondary = document.querySelector("#secondary");
-          if (secondary){
-            setTimeout(() => {
-              secondary.prepend(iframe);
-            }, 4000)
-            observer.disconnect();  
-          }
-          break;
-      }
-  });
+//this mutation observer for detecting URL changes comes from this SO: https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+const observeUrlChange = () => {
+  let oldHref = document.location.href;
+  const body = document.querySelector('body');
+  const observer = new MutationObserver(mutations => {
+    if (oldHref !== document.location.href) {
+      oldHref = document.location.href;
 
- };
-const observer = new MutationObserver(callback);
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-
-observer.observe(document.getElementById("page-manager"), config);
-
-// Interact with video player on YouTube
-var video;
-
-const playerCallback = (mutationsList, observer) => {
-  mutationsList.forEach(mutation => {
-    switch(mutation.type) {
-      case 'childList':
-        video = document.querySelector('video')
-        if (video){
-          observer.disconnect()
-        }
-        break;
+      // Changes
+      const secondary = document.querySelector("#secondary");
+      secondary.removeChild(document.getElementById('summify-frame')) //remove iframe currently on screen
+      videoID = window.location.href
+      videoID = videoID.substring(videoID.indexOf("v=")+2, videoID.length)
+      iframePrepened = false
+      iframe.src = 'http://127.0.0.1:5000/yt_url/' + videoID;
+      setTimeout(() => {
+        secondary.prepend(iframe)
+      }, 2500)
+      // observer = new MutationObserver(callback);
+      // playerObserver = new MutationObserver(playerCallback);
+      // observer.observe(document.getElementById("content"), config);
+      // playerObserver.observe(document.getElementById("content"), config);
     }
-  })
-}
+  });
+  observer.observe(body, { childList: true, subtree: true });
+};
 
-const playerObserver = new MutationObserver(playerCallback);
-playerObserver.observe(document.getElementById("page-manager"), config);
+window.onload = observeUrlChange;
 
 // Listen for requests to change video timestamp
 window.addEventListener('message', (event) => {
